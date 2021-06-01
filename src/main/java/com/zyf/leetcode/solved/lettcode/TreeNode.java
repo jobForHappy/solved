c9a1e018294ff117b5619366c15cade3096aecda
张云飞(003361) <zhangyunfei@fcbox.com>
2021-06-01 18:11:46 +0800

    树的基本结构类

---
+++ src/main/java/com/zyf/leetcode/solved/lettcode/TreeNode.java
@@ -0,0 +1,22 @@
+package com.zyf.leetcode.solved.lettcode;
+
+/**
+ * @Title: TreeNode.java
+ * @Package com.zyf.leetcode.solved.lettcode
+ * @Description: 树
+ * @Author: Administrator
+ * @Date: 2021/6/1 18:10
+ * @Version V1.0
+ * @License: Copyright Since 2015 Hive Box Technology. All rights reserved.
+ * @Notice: This content is limited to the internal circulation of Hive Box, and it is prohibited to leak or used for other commercial purposes.
+ */
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
